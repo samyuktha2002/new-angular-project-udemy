b97a5d2157c899940ee7431152570e6de26c7fd0
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2025-12-31 11:19:20 +0530

    Revert "Using signal inputs"
    This reverts commit c80a25538f4426cdc9cf81ef85a4bbc79d55d034.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,19 +8,12 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  // get imagePath(){
-  //   return 'assets/users/' + this.avatar();
-  // }
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser(){
   }
