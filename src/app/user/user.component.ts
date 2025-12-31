962946ca36c80bcd7ae184b6605523290eb76a4f
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2025-12-31 11:42:10 +0530

    Revert "using the output() function"
    This reverts commit ef0516336c6b397245165523ea4839e1c3afa4f7.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,8 +12,7 @@ export class UserComponent {
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
 
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
