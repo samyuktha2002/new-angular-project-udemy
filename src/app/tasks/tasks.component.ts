10c5479170b54f71c58d11980dd48af4f3843b12
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2025-12-31 13:05:14 +0530

    setting required true

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -8,6 +8,6 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({required: true}) name?: string;
 
 }
